2a6837c878f06a23052c71169bc58c1419542437
Ketho <sghs17@outlook.com>
2021-07-03 01:36:33 +0200

    Repurpose for local testing

--- src/extension.ts
+++ src/extension.ts
@@ -6,26 +6,13 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 export function setExternalLibrary(folder: string, enable: boolean) {
-	console.log("setExternalLibrary", folder, enable)
-	const extensionId = "publisher.name"
-	const extensionPath = vscode.extensions.getExtension(extensionId)?.extensionPath
-	const folderPath = extensionPath+"\\"+folder
 	const config = vscode.workspace.getConfiguration("Lua")
 	const library: string[] | undefined = config.get("workspace.library")
-	if (library && extensionPath) {
-		// remove any older versions of our path e.g. "publisher.name-0.0.1"
-		for (let i = library.length-1; i >= 0; i--) {
-			const el = library[i]
-			const isSelfExtension = el.indexOf(extensionId) > -1
-			const isCurrentVersion = el.indexOf(extensionPath) > -1
-			if (isSelfExtension && !isCurrentVersion) {
-				library.splice(i, 1)
-			}
-		}
-		const index = library.indexOf(folderPath)
+	if (library) {
+		const index = library.indexOf(folder)
 		if (enable) {
 			if (index == -1) {
-				library.push(folderPath)
+				library.push(folder)
 			}
 		}
 		else {
